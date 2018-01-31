2da2e7b7ecb485e20d521b8045247bd758a55a7a
bill wood <billwood.dev@gmail.com>
2018-01-30 22:05:06 -0500

    Initial simple server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.set('port', (process.env.PORT || 9001));
+
+/* ROUTES */
+app.get('/',function(req,res){
+	res.send('We are up and running');
+});
+/** END ROUTES **/
+
+app.listen(app.get('port'), function(){
+	Console.log('Node is listening on port', app.get('port'));
+});
