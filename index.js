fd887c56a46d231fb674be75ceae948f70cc528c
bill wood <billwood.dev@gmail.com>
2018-01-31 20:43:16 -0500

    Send Oauth temp code for AuthToken in /slack/auth route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -11,15 +12,42 @@ app.set('port', (process.env.PORT || 9001));
 
 /* ROUTES */
 app.get('/',function(req,res){
-	res.send('We are up and running');
-});
-
-app.get('/dm', function(req,res){
 	res.sendFile(__dirname+'/app/index.html');
 });
 
+app.get('/dm', function(req,res){
+	res.send('DM Screen');
+});
+
 app.get('/slack/auth', function(req, res){
-	res.send('simple auth');
+	let options;
+	
+	if(req.query.code){
+
+		options = {
+			url: 'https://slack.com/api/oauth.access',
+			method: 'GET',
+			form: {
+				client_id: process.env.client_id,
+				client_secret: process.env.client_secret,
+				code: req.query.code
+			}
+		};
+
+		request(options, function (error, response, content) {
+			if (!error && response.statusCode == 200) {
+
+			// Print out the response body
+			console.log('We got something back');
+			console.log(content);
+			
+			res.sendFile(__dirname+'/app/index.html');
+			}
+		});
+	}    
+	else{
+    	res.sendFile(__dirname+'/app/error.html');
+	}
 });
 /** END ROUTES **/
 
